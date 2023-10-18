349e9c096697e1ad5546d2d6d365343d1878105b
FrancisD99 <akhenatondouniama@gmail.com>
2023-10-18 18:57:52 +0100

    refresh link to pictures.

--- SRC/app.js
+++ SRC/app.js
@@ -1,15 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
-
 /*  REGLES DU JEU:
 
 - Le jeu se déroule à 2 joueurs, jouant en tours
@@ -31,7 +19,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         //2. Display the result
         const diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        diceDOM.src = 'asset/img/dice-' + dice + '.png';
 
 
         //3. Update the round score IF the rolled number was NOT a 1
